0a91e4a255571102b89363c1c12d9b4710007e2f
Vivek <vivektheintel@gmail.com>
2021-01-03 13:03:18 +0530

    Making tts key pluggable

--- bin/pdf_tts.sh
+++ bin/pdf_tts.sh
@@ -5,5 +5,5 @@ JAVA_PARAM="-Xmx1g"
 BIN_PATH=$TWM_HOME_PARENT/$APP_NAME/bin
 JAR_PATH=$BIN_PATH/../target/$APP_NAME-$APP_VERSION.jar
 
-echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', at '$JAR_PATH'"
-java $JAVA_PARAM -Dtts.key=QDW3VUiN -jar $JAR_PATH
+echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', at '$JAR_PATH', TTS_KEY: '$TTS_KEY'"
+java $JAVA_PARAM -Dtts.key=$TTS_KEY -jar $JAR_PATH
