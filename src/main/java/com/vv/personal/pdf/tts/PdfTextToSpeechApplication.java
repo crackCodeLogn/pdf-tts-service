e1973b855d849d2643af593d360215afcfd8a2fb
Vivek <vivektheintel@gmail.com>
2021-01-03 23:03:31 +0530

    Standardizing pdf-tts-service

--- src/main/java/com/vv/personal/pdf/tts/PdfTextToSpeechApplication.java
+++ src/main/java/com/vv/personal/pdf/tts/PdfTextToSpeechApplication.java
@@ -1,16 +1,51 @@
 package com.vv.personal.pdf.tts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static com.vv.personal.pdf.tts.constants.Constants.*;
 
 @SpringBootApplication
 public class PdfTextToSpeechApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PdfTextToSpeechApplication.class);
+    @Autowired
+    private Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(PdfTextToSpeechApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.vv.personal.pdf.tts"))
+                .build();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void firedUpAllCylinders() {
+        String host = LOCALHOST;
+        try {
+            host = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            LOGGER.error("Failed to obtain ip address. ", e);
+        }
+        String port = environment.getProperty(LOCAL_SPRING_PORT);
+        LOGGER.info("DBI activation is complete! Exact url: {}", String.format(SWAGGER_UI_URL, host, port));
     }
 
 }
--- src/main/java/com/vv/personal/pdf/tts/constants/Constants.java
+++ src/main/java/com/vv/personal/pdf/tts/constants/Constants.java
@@ -24,4 +24,9 @@ public class Constants {
     public static final String REQ_PARAM_TTS_GENDER = "gender";
     public static final String REQ_PARAM_TTS_TEXT = "text";
     public static final String PREFIX_DOWNLOADED_SPEECH_LINE_PARTS = "%s-%d-";
+
+    public static final String LOCALHOST = "localhost";
+    public static final String SWAGGER_UI_URL = "http://%s:%s/swagger-ui/index.html";
+    public static final String LOCAL_SPRING_HOST = "local.server.host";
+    public static final String LOCAL_SPRING_PORT = "local.server.port";
 }
